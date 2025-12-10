9593f6ce8bb2d3ecda99009ed9b17b1376822391
hemant-7k <155747419+hemant-7k@users.noreply.github.com>
2025-12-10 15:13:15 +0000

    variables talk

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const  accountId = 123444
+let accountEmail = "hemantkoranga@gmail.com"
+var accountpassward = "123456" 
+/*
+prefer not to use var
+because of issue in block scope anf functional scope 
+*/
+
+accountcity ="jaipur" /*In javascrip we can also reserve the space as virable without usint const or let . but this is not a way of declearing variable */
+let accountstate;
+
+
+accountEmail = "hemantkoranga@gmail...com"
+accountpassward = "12345544"
+accountcity = "delhi"
+
+
+
+console.table([accountId, accountEmail, accountpassward, accountcity, accountstate])
